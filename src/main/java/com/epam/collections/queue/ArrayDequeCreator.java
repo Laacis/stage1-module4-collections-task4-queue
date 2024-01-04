08890adff9c686cd5c851f2a55c3bb42142a3746
Jurijs <j.lacis.code@gmail.com>
2024-01-04 21:03:54 +0800

    complete

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,44 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+
+        boolean queueHasValue = firstQueue.size() >= 2 && secondQueue.size() >= 2;
+
+        giveTwoCards(result, firstQueue);
+        giveTwoCards(result, secondQueue);
+
+        while(queueHasValue && !firstQueue.isEmpty() && !secondQueue.isEmpty() ){
+            takeOneAndAddLast(firstQueue, result);
+            giveTwoCards(result, firstQueue);
+
+            takeOneAndAddLast(secondQueue, result);
+            queueHasValue = giveTwoCards(result, secondQueue);
+        }
+
+        return  result;
     }
+
+    private void takeOneAndAddLast(Queue<Integer> to, ArrayDeque<Integer> from) {
+        Integer card = from.getLast();
+        from.removeLast();
+        to.add(card);
+    }
+
+    private boolean giveTwoCards(ArrayDeque<Integer> to, Queue<Integer> from) {
+        int cards = 2;
+        while (cards > 0){
+            Integer x = from.poll();
+
+            if( x == null){
+                return false;
+            }
+
+            to.add(x);
+            cards -= 1;
+        }
+
+        return true;
+    }
+
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,44 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        LinkedList<Integer> dishes = makeArray(numberOfDishes);
+
+        int counter = everyDishNumberToEat -1;
+        List<Integer> numbers = new ArrayList<>();
+
+        while(result.size() < numberOfDishes){
+
+            for(; counter < dishes.size(); counter += everyDishNumberToEat){
+                numbers.add(dishes.get(counter));
+            }
+
+            counter -= dishes.size();
+            result.addAll(numbers);
+            removeNumbersFromList(dishes, numbers);
+            numbers.clear();
+        }
+
+        return  result;
+    }
+
+    private void removeNumbersFromList(LinkedList<Integer> list, List<Integer> numbers) {
+        for(Integer number : numbers){
+            list.remove(number);
+        }
+    }
+
+    private  LinkedList<Integer> makeArray(int arrayLength) {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        for(int i = 1; i<= arrayLength; i++){
+            result.add(i);
+        }
+        return  result;
     }
 }
